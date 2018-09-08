621f5cb2456b521478233191bb9e2d65fae78c7d
guangfeng.wan <guangfeng.wan@taotu-partner.com>
2018-09-08 15:54:26 +0800

    version 2.0 zookeeper discovery

--- src/main/java/com/example/springBootAdmin/SpringBootAdminApplication.java
+++ src/main/java/com/example/springBootAdmin/SpringBootAdminApplication.java
@@ -3,9 +3,11 @@ package com.example.springBootAdmin;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableAdminServer
+@EnableDiscoveryClient
 public class SpringBootAdminApplication {
 
 	public static void main(String[] args) {
