8f3d83d9ec62c5c41674d3329cb4738c5a84e4c2
guangfeng.wan <guangfeng.wan@taotu-partner.com>
2018-09-15 15:05:43 +0800

    add 集群

--- src/main/java/com/example/springBootAdmin/SpringBootAdminApplication.java
+++ src/main/java/com/example/springBootAdmin/SpringBootAdminApplication.java
@@ -1,14 +1,27 @@
 package com.example.springBootAdmin;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.config.EvictionPolicy;
+import com.hazelcast.config.ListConfig;
+import com.hazelcast.config.MapConfig;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableAdminServer
 @EnableDiscoveryClient
 public class SpringBootAdminApplication {
+	@Bean
+	public Config hazelcastConfig() {
+		return new Config().setProperty("hazelcast.jmx", "true")
+				.addMapConfig(new MapConfig("spring-boot-admin-application-store").setBackupCount(1)
+						.setEvictionPolicy(EvictionPolicy.NONE))
+				.addListConfig(new ListConfig("spring-boot-admin-event-store").setBackupCount(1)
+						.setMaxSize(1000));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAdminApplication.class, args);
